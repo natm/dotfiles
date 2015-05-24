2e1ba2bd3cf302ce9f4c97fff5c4d38c078af3d6
Nat Morris <nat@nuqe.net>
2015-05-24 21:23:06 +0100

    possibly use cask to install apps

--- setup_mac.sh
+++ setup_mac.sh
@@ -12,4 +12,11 @@ ln -s $PWD/vimrc $HOME/.vimrc
 rm $HOME/.ssh/config
 ln -s $PWD/sshconfig $HOME/.ssh/config
 
+brew install mtr
+# brew install Caskroom/cask/google-chrome
+
+
+sudo chown root:wheel /usr/local/Cellar/mtr/0.86/sbin/mtr
+sudo chmod u+s /usr/local/Cellar/mtr/0.86/sbin/mtr
+
 exit 0
