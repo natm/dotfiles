4c346493b5c05cb61774dccc55d8c811cddedcb5
Nat Morris <nat@netflix.com>
2015-06-04 09:37:05 -0700

    also install atom

--- setup_mac.sh
+++ setup_mac.sh
@@ -164,6 +164,7 @@ dockutil --remove "Safari"
 dockutil --add /opt/homebrew-cask/Caskroom/google-chrome/latest/Google\ Chrome.app
 dockutil --add /opt/homebrew-cask/Caskroom/iterm2/2.0/iTerm.app
 dockutil --add /opt/homebrew-cask/Caskroom/hipchat/latest/HipChat.app
+dockutil --add /opt/homebrew-cask/Caskroom/atom/latest/Atom.app
 dockutil --move "Google Chrome" --position 1
 
 exit 0
